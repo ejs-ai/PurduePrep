22d735972d971eb6d9d2935a477397fdf223936f
skrichef <skrichef@purdue.edu>
2024-09-18 15:29:07 -0400

    Addressing changes + unit test

--- PurduePrep/webcrawl/extract_body_text.py
+++ PurduePrep/webcrawl/extract_body_text.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from pypdf import PdfReader, PdfWriter
 import sys
 import os
-from webcrawl_functions import open_url, url_screen_for_pdf, write_out_text_file
+from webcrawl_functions import open_url, url_screen_for_pdf, write_out_text_file, get_webcrawl_functions_path
 
 ece404_url = 'https://weeklyjoys.wordpress.com/wp-content/uploads/2021/10/ece404_e1_sp2021.pdf'
 pdf_name, out_text_file, pdf_hex_content = url_screen_for_pdf(ece404_url)
--- PurduePrep/webcrawl/webcrawl_functions.py
+++ PurduePrep/webcrawl/webcrawl_functions.py
@@ -1,14 +1,14 @@
 import requests
-from bs4 import BeautifulSoup
-import pandas as pd
 from pypdf import PdfReader, PdfWriter
 import re
 import sys
+from pathlib import Path
 
 def open_url(url_to_scrape):
     url_content = requests.get(url = url_to_scrape, headers = {'User-Agent': "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"})
     if not url_content.ok:
-        sys.exit("Could not read content at: "+url_to_scrape)
+        print("Could not read content at: "+url_to_scrape)
+        url_content = -1
     return url_content
 
 def url_screen_for_pdf(url):
@@ -17,8 +17,20 @@ def url_screen_for_pdf(url):
     pdf_hex_content = open_url(url)
     return pdf_name, out_text_file, pdf_hex_content
 
+def get_webcrawl_functions_path():
+    current_dir = Path(__file__).resolve()
+    for parent in current_dir.parents:
+        if parent.name == 'PurduePrep':
+            purdue_prep_dir = parent
+            break
+    else:
+        raise FileNotFoundError("Could not locate the 'PurduePrep' directory")
+    webcrawl_functions_path = purdue_prep_dir / 'webcrawl'
+    return str(webcrawl_functions_path)
+
 def write_out_text_file(file_to_write, pdf_content):
-    with open(file_to_write, "w", encoding="utf-8") as output_file_with_content:
+    path_to_file = get_webcrawl_functions_path()
+    with open(path_to_file + "\\" + file_to_write, "w", encoding="utf-8") as output_file_with_content:
         for page_num in range(len(pdf_content.pages)):
             page = pdf_content.pages[page_num]
             page_text = page.extract_text()
