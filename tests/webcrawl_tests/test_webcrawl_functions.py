40110524597d9d3d4da8d851b86a2302c88a5340
skrichef <skrichef@purdue.edu>
2024-09-18 18:56:14 -0400

    testing automated build

--- tests/webcrawl_tests/test_webcrawl_functions.py
+++ tests/webcrawl_tests/test_webcrawl_functions.py
@@ -2,10 +2,8 @@ import sys
 import os
 from webcrawl_functions import open_url
 
-
 webcrawl_functions_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'PurduePrep', 'webcrawl'))
 sys.path.insert(1, webcrawl_functions_path)
-
 
 def test_open_url_good_link():
     test_url = 'https://weeklyjoys.wordpress.com/wp-content/uploads/2021/10/ece404_e1_sp2021.pdf'
