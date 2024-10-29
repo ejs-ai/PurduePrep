8975ab2fbcd56640e5a4b67d67171318d937298e
Joshua Kamphuis <jpkamphu@purdue.edu>
2024-10-29 19:48:11 -0400

    imports

--- PurduePrep/webcrawl/crawler.py
+++ PurduePrep/webcrawl/crawler.py
@@ -3,8 +3,8 @@ import requests
 # from welcrawl.webcrawl_functions import get_content_from_pdf_link, get_websites, init_gather_websites, crawl
 # from webcrawl.page import Page
 
-from webcrawl_functions import get_content_from_pdf_link, init_gather_websites, crawl_websites
-from page import Page
+from PurduePrep.webcrawl.webcrawl_functions import get_content_from_pdf_link, init_gather_websites, crawl_websites
+from PurduePrep.webcrawl.page import Page
 import time
 
 # Input from Emma
