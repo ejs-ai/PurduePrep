8a8339e2d514ee3dc9528edbdf0ce9ef0353f8dd
Joshua Kamphuis <jpkamphu@purdue.edu>
2024-10-29 21:39:17 -0400

    .tsx debugging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -118,7 +118,7 @@ const Home: React.FC = () => {
 const styles = {
   container: {
     display: 'flex',
-    flexDirection: 'column' as 'column',
+    flexDirection: 'column' as const,
     justifyContent: 'center',
     alignItems: 'center',
     height: '100vh',
@@ -142,7 +142,7 @@ const styles = {
   },
   columns: {
     display: 'flex',
-    flexDirection: 'row' as 'row',
+    flexDirection: 'row' as const,
     justifyContent: 'space-between',
     width: '80%',  // Adjust the width as needed
   },
