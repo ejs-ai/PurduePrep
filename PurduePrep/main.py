18c2c758fc5d836cc84c28c7fa16da053f784236
eteff <eteff@purdue.edu>
2024-11-19 19:53:46 -0500

    main.py returns after built query

--- PurduePrep/main.py
+++ PurduePrep/main.py
@@ -24,11 +24,14 @@ def main(user_input, num_questions):
     # Input handler ---(string)---> Query builder
     # Step 3: Query builder processes all user's gibberish, extracts relevant info, forms query string
     query_string = build_query(user_input)
+    return [("query string", query_string)]
 
     # Query builder ---(query string)---> Web crawler
     # Step 4: Web crawler uses the query string to produce a list of relevant websites
     websites_list, all_page_scores = init_gather_websites(query_string)
+    return [("gathered websites", "gathered not crawled")]
     sorted_relevant_urls = crawl_websites(query_string, websites_list, CRAWL_DEPTH, all_page_scores)
+    return [("gathered websites", "gathered not crawled")]
     # Web crawler ---(relevant sites list)---> Scraper
     # Step 5: Web scraper extracts information from websites
     questions = []
